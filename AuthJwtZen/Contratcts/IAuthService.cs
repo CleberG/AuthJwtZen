9b9b1339df3e2b3fcfe604e2dd0edb38ac46ac43
Cleber Gomes <cgmbnu@gmail.com>
2025-08-06 05:39:47 -0300

    Inciado lib para autenticação

---
+++ AuthJwtZen/Contratcts/IAuthService.cs
@@ -0,0 +1,29 @@
+﻿using AuthJwtZen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthJwtZen.Contratcts
+{
+	/// <summary>
+	/// Contrato principal para as operações de registro e login de usuários.
+	/// </summary>
+	public interface IAuthService
+	{
+		/// <summary>
+		/// Registra um novo usuário no sistema.
+		/// </summary>
+		/// <param name="request">Dados para registro do novo usuário.</param>
+		/// <returns>Um AuthResponse contendo o resultado da operação.</returns>
+		Task<AuthResponse> RegisterAsync(RegisterRequest request);
+
+		/// <summary>
+		/// Autentica um usuário e retorna um token JWT se for bem-sucedido.
+		/// </summary>
+		/// <param name="request">Credenciais de login do usuário.</param>
+		/// <returns>Um AuthResponse contendo o token e o resultado da operação.</returns>
+		Task<AuthResponse> LoginAsync(LoginRequest request);
+	}
+}
---
+++ AuthJwtZen/Contratcts/ITokenService.cs
@@ -0,0 +1,22 @@
+﻿using AuthJwtZen.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthJwtZen.Contratcts
+{
+	/// <summary>
+	/// Contrato para o serviço que gera tokens de autenticação.
+	/// </summary>
+	internal interface ITokenService
+	{
+		/// <summary>
+		/// Gera um token JWT para um usuário específico.
+		/// </summary>
+		/// <param name="user">O usuário para o qual o token será gerado.</param>
+		/// <returns>Uma string representando o token JWT.</returns>
+		string GenerateToken(UserBase user);
+	}
+}
